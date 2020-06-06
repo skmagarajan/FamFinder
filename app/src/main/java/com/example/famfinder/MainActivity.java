79a56a4d507d2bd238ed052e2f940d00753a6e86
saro-mano <contactsaravanm@gmail.com>
2020-06-06 01:13:28 -0700

    Added SignUp Page

--- app/src/main/java/com/example/famfinder/MainActivity.java
+++ app/src/main/java/com/example/famfinder/MainActivity.java
@@ -15,11 +15,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button login = (Button) findViewById(R.id.login);
+        Button signup = (Button) findViewById(R.id.signup);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent Page = new Intent(MainActivity.this,Options.class);
+                startActivity(Page);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Page = new Intent(MainActivity.this,SignUp.class);
                 startActivity(Page);
             }
         });
