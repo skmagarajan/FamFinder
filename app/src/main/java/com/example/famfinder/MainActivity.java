31e7f04723fb90aa1b37c41093eb09e165c70683
saro-mano <contactsaravanm@gmail.com>
2020-08-18 23:14:30 -0700

    Google Sign in Added

--- app/src/main/java/com/example/famfinder/MainActivity.java
+++ app/src/main/java/com/example/famfinder/MainActivity.java
@@ -11,6 +11,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
@@ -24,7 +30,46 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 0;
     private String TAG = "Login Page";
+    SignInButton g_signin;
+    GoogleSignInClient mGoogleSignInClient;
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            System.out.println("Success");
+            Intent Options = new Intent(MainActivity.this,Options.class);
+            Bundle b = new Bundle();
+            b.putString("MailID","Saravanan");
+            Options.putExtras(b);
+            startActivity(Options);
+            Toast.makeText(getApplicationContext(),"Opening",Toast.LENGTH_LONG).show();
+        } catch (ApiException e) {
+
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +81,25 @@ public class MainActivity extends AppCompatActivity {
         final EditText mail = (EditText) findViewById(R.id.mailid);
         final EditText password = (EditText) findViewById(R.id.password);
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        g_signin = findViewById(R.id.sign_in_button);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        g_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                    // ...
+                }
+            }
+        });
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
